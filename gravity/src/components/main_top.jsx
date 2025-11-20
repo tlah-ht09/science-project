e0c12602e58e3d73451b7372b514b051bd07e1ff
이호준 <hjsj23430@naver.com>
2025-11-21 08:42:48 +0900

    아으야

---
+++ gravity/src/components/main_top.jsx
@@ -0,0 +1,14 @@
+import * as _ from './main_top.js'
+
+export const MainTop = () => {
+    return(
+        <_.top>
+            <_.logo>
+                자유낙하 시각화
+            </_.logo>
+            <_.logButton>
+                로그인
+            </_.logButton>
+        </_.top>
+    )
+}
