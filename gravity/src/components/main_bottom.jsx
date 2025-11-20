bfaa51fd5ba2577f7f6a6b33cc68a0e099f07f6e
정현태 <jht3021@gmail.com>
2025-11-21 08:46:40 +0900

    테스트 병합을 위한 커밋

---
+++ gravity/src/components/main_bottom.jsx
@@ -0,0 +1,11 @@
+import * as _ from "./main_bottom";
+
+export const Main_bottom = () => {
+  return (
+    <_.Main>
+      <_.Main_in>
+        <_.p>avc</_.p>
+      </_.Main_in>
+    </_.Main>
+  );
+};
