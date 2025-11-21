7b1227b224a489480432544f1f285d9c9926f497
정현태 <jht3021@gmail.com>
2025-11-21 09:11:33 +0900

    푸터 완성

--- gravity/src/components/main_bottom.jsx
+++ gravity/src/components/main_bottom.jsx
@@ -1,5 +1,10 @@
 import * as _ from "./main_bottom";
 
 export const Main_bottom = () => {
-  return <_.Main>d</_.Main>;
+  return (
+    <_.Main>
+      <_.Text>@made by 곰돌이미역 먹는 말미잘</_.Text>
+      <_.blackLiks>블랙리스트</_.blackLiks>
+    </_.Main>
+  );
 };
