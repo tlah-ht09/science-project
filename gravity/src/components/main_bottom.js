c1a60de0a43514f91888cad32d921c4a5b22446d
정현태 <jht3021@gmail.com>
2025-11-21 09:11:33 +0900

    푸터 완성

--- gravity/src/components/main_bottom.js
+++ gravity/src/components/main_bottom.js
@@ -1,11 +1,40 @@
 import styled from "@emotion/styled";
 
 export const Main = styled.div`
-  background-color: red;
+  width: 1300px;
+  height: 50px;
 
+  display: flex;
+  width: 1370px;
+  padding: 15px 18px;
+
+  align-items: flex-start;
+  gap: 56%;
+  border-top: 1px solid #d1d1d1;
+`;
+
+export const Text = styled.div`
   color: #000;
   font-family: Jua;
   font-size: 30px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+
+export const blackLiks = styled.button`
+  display: flex;
+  width: 184px;
+  padding: 4px 32px;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  border-radius: 10px;
+  background: #e90000;
+  border: none;
+
+  font-family: Jua;
+  font-size: 24px;
   font-style: normal;
   font-weight: 400;
   line-height: normal;
