8a2c2e716b8754f5ccb8a6b4ba47cc4ab18622a8
정현태 <jht3021@gmail.com>
2025-11-29 21:59:56 +0900

    구글 로그인 후 사용자 이름과 이메일 얻어오기 완성

---
+++ gravity/src/components/logIn_button.js
@@ -0,0 +1,20 @@
+import styled from "@emotion/styled";
+
+export const logButton = styled.button`
+  display: flex;
+  padding: 9px 35px 9px 34px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+  background: #000;
+  color: #fff;
+  font-family: "Jua";
+  font-size: 30px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
--- gravity/src/components/main_top.jsx
+++ gravity/src/components/main_top.jsx
@@ -1,14 +1,11 @@
-import * as _ from './main_top.js'
+import { CustomGoogleButton } from "./logIn_button.jsx";
+import * as _ from "./main_top.js";
 
 export const MainTop = () => {
-    return(
-        <_.top>
-            <_.logo>
-                자유낙하 시각화
-            </_.logo>
-            <_.logButton onClick={()=>{}}>
-                로그인
-            </_.logButton>
-        </_.top>
-    )
-}
+  return (
+    <_.top>
+      <_.logo>자유낙하 시각화</_.logo>
+      <CustomGoogleButton></CustomGoogleButton>
+    </_.top>
+  );
+};
