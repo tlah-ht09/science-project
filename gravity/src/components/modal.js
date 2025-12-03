d6a484be01b3b8ee8a8f2303e82eb2b050ee1f0c
이호준 <hjsj23430@naver.com>
2025-12-03 10:26:28 +0900

    모달창 수정

--- gravity/src/components/modal.js
+++ gravity/src/components/modal.js
@@ -1,10 +1,13 @@
 import styled from '@emotion/styled'
 
+
+
 export const main = styled.div`
     width: 1100px;
     height: 700px;
     border-radius: 20px;
     background: #FFF;
+    
 `
 
 export const top = styled.div`
@@ -35,7 +38,8 @@ export const buttom = styled.div`
     align-items: flex-start;
     gap: 10px;
     border-radius: 0 0 20px 20px;
-    border-top: 1px solid #D1D1D1;    
+    border-top: 1px solid #D1D1D1;  
+    background-color: white;  
 `
 
 export const buttomMid = styled.div`
@@ -54,13 +58,13 @@ export const buttomMidT = styled.p`
     line-height: normal;
 `
 
-export const buttomButtomDiv = styled.div`
+export const buttomButtonDiv = styled.div`
     display: flex;
     align-items: center;
     gap: 30px;
 `
 
-export const buttomBtton = styled.button`
+export const buttomBtton = styled.input`
     display: flex;
     padding: 10px 23px;
     justify-content: center;
@@ -69,6 +73,12 @@ export const buttomBtton = styled.button`
     border-radius: 10px;
     border: 1px solid #929292;
     background: #3B3B3B;
+    color: #FFF;
+    font-family: Inter;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
 `
 
 export const buttomBtton2 = styled.button`
@@ -79,7 +89,13 @@ export const buttomBtton2 = styled.button`
     gap: 10px;
     border-radius: 10px;
     border: 1px solid #929292;
-    background: #D6D6D6;
+    background: #3B3B3B;
+    color: #FFF;
+    font-family: Inter;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
 `
 export const buttomBtton3 = styled.button`
     display: flex;
@@ -88,12 +104,36 @@ export const buttomBtton3 = styled.button`
     align-items: center;
     gap: 10px;
     border-radius: 10px;
+    border: 1px solid #929292;
+    background: #D6D6D6;
+    color: #000;
+    font-family: Inter;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+`
+
+export const buttomBtton4 = styled.button`
+   display: flex;
+    padding: 10px 23px;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    border-radius: 10px;
     background: #E1E1E1;
+    color: #000;
+    font-family: Inter;
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
 `
 
 export const mid = styled.div`
-    display: inline-flex;
-    padding: 20px 0 20px 30px;
+    width:978px;
+    heigth: 455px;
+    padding: 20px 0px 20px 30px;
     flex-direction: column;
     align-items: flex-start;
     gap: 20px;
@@ -111,7 +151,7 @@ export const midMid = styled.div`
 
 export const midMidMid = styled.div`
     display: flex;
-    width: 948px;
+    height: 455px;
     justify-content: space-between;
     align-items: center;
 `
@@ -142,8 +182,7 @@ export const midMidMidLeftT = styled.p`
 
 export const midMidMidLeftScreen = styled.div`
     display: flex;
-    height: 391px;
-    padding: 46px 0 307px 0;
+    height: 400px;
     justify-content: center;
     align-items: center;
     align-self: stretch;
@@ -159,26 +198,25 @@ export const midMidMidRight = styled.div`
 
 export const midMidMidRightlineDiv = styled.div`
     display: flex;
-    width: 62px;
+    width: 30px;
     flex-direction: column;
     align-items: center;
 
 `
 
 export const midMidMidRightlineDivT = styled.p`
-    align-self: stretch;
     color: #000;
     font-family: Jua;
     font-size: 15px;
     font-style: normal;
     font-weight: 400;
     line-height: normal;
-    border-bottom : 1px black solid;
 `
 
 export const midMidMidline = styled.div`
-    width: 62px;
-    height: 65px;
+    width: 50px;
+    height: 35px;
     border-bottom : 1px black solid;
+    align-items: right;
     
 `
