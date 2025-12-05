d90207a79d755596b6915a91b343493f79078b2e
이호준 <hjsj23430@naver.com>
2025-12-05 23:26:28 +0900

    블랙리스트 모달 추가

--- gravity/src/components/main_bottom.jsx
+++ gravity/src/components/main_bottom.jsx
@@ -1,12 +1,31 @@
-import * as _ from "./main_bottom";
+import { useState } from "react";
+import * as _ from "./main_bottom"; 
+import {Modal2} from "./modal2.jsx";
+
 
 export const Main_bottom = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
-    <_.Main>
-      <_.Text>@made by 곰돌이미역 먹는 말미잘</_.Text>
-      <_.blackLiks>블랙리스트</_.blackLiks>
-    </_.Main>
+    <>
+      <_.Main>
+        <_.Text>@made by 곰돌이미역 먹는 말미잘</_.Text>
+        <_.blackLiks onClick={handleOpenModal}>
+          블랙리스트
+        </_.blackLiks>
+      </_.Main>
+
+      {isModalOpen && (
+            <Modal2></Modal2>
+        )}
+    </>
   );
 };
-
-<input ></input>
---
+++ gravity/src/components/modal2.jsx
@@ -0,0 +1,12 @@
+import { useEffect, useState } from 'react'
+import * as _ from './modal2.js'
+
+export const Modal2 = () => {
+    return (
+        <>
+        <_.Modal>
+            <_.ModalBackground></_.ModalBackground>
+        </_.Modal>
+        </>
+    )
+}
